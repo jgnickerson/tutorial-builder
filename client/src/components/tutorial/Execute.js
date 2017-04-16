7c5481c685d5c1d17bf5c04d8db4e55f4e47c9a9
jgnickerson <jgnickerson@me.com>
2017-04-16 18:09:13 -0400

    execution console.logs append to document body

--- client/src/components/tutorial/Execute.js
+++ client/src/components/tutorial/Execute.js
@@ -5,12 +5,31 @@ import React, { Component } from 'react';
 
 function myFunc(code, iframe) {
     let doc;
+
+
+    let otherCode = `
+    if (''+baseLogFunction !== ''+parent.console.log) {
+      var baseLogFunction = console.log;
+    }
+
+    console.log = function(){
+      baseLogFunction.apply(console, arguments);
+
+      var args = Array.prototype.slice.call(arguments);
+      for(var i=0;i<args.length;i++){
+          parent.logEntry(args[i]);
+      }
+    }
+    `
+
+    code = otherCode + code;
     //just putting in JS code for now becuase that is the only code editor we have
     let result = '<html><head>' + '<style>' + '</style></head><body>' + '<script type="text/javascript">' + code + '</script></body></html>';
 
     //determining which part of document to grab
     if(iframe.contentDocument) {
         doc = iframe.contentDocument;
+    //this is a workaround for Internet Explorer 8
     } else if(iframe.contentWindow) {
         doc = iframe.contentWindow.document;
     } else {
@@ -19,8 +38,15 @@ function myFunc(code, iframe) {
 
     //writing file in the iframe and compiling
     doc.open();
-    doc.writeln(result);
+    doc.write(result);
     doc.close();
+}
+
+window.logEntry = function(message) {
+  let node = document.createElement('div');
+  let text = document.createTextNode(message);
+  node.appendChild(text);
+  document.body.appendChild(node);
 }
 
 function Execute(props){
