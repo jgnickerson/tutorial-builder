aea84e5abf019aa7572d872fac388e7424ca91b7
jgnickerson <jgnickerson@me.com>
2017-04-16 21:07:31 -0400

    old logs removed upon new execution

--- client/src/components/tutorial/Execute.js
+++ client/src/components/tutorial/Execute.js
@@ -3,73 +3,78 @@
 */
 import React, { Component } from 'react';
 
-function myFunc(code, iframe) {
-    let doc;
+//Execute Component
+function Execute(props){
+    return <div><button onClick={()=>executeCode(props.code, false)}>Run</button></div>
+}
 
 
-    let otherCode = `
-    if (''+baseLogFunction !== ''+parent.console.log) {
-      var baseLogFunction = console.log;
+function executeCode(code, showIframe) {
+    //if an iFrame already exists, remove it
+    if (document.getElementById("iframe")) {
+        let iframe = document.getElementById("iframe");
+        iframe.parentNode.removeChild(iframe);
     }
 
-    console.log = function(){
-      baseLogFunction.apply(console, arguments);
+    //create a new iframe
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute("id","iframe")
+    document.body.appendChild(iframe);
 
-      var args = Array.prototype.slice.call(arguments);
-      for(var i=0;i<args.length;i++){
-          parent.logEntry(args[i]);
+    if (!showIframe) {
+      iframe.style.display = 'none';
+    }
+
+    refreshLogs();
+
+    //this code redirects all console.log in user code to a function of ours (logEntry)
+    let consoleLogRewrite = `
+      if (''+baseLogFunction !== ''+parent.console.log) {
+        var baseLogFunction = console.log;
       }
-    }
-    `
 
-    code = otherCode + code;
-    //just putting in JS code for now becuase that is the only code editor we have
-    let result = '<html><head>' + '<style>' + '</style></head><body>' + '<script type="text/javascript">' + code + '</script></body></html>';
+      console.log = function(){
+        baseLogFunction.apply(console, arguments);
 
-    //determining which part of document to grab
-    if(iframe.contentDocument) {
-        doc = iframe.contentDocument;
-    //this is a workaround for Internet Explorer 8
-    } else if(iframe.contentWindow) {
-        doc = iframe.contentWindow.document;
-    } else {
-        doc = iframe.document;
-    }
+        var args = Array.prototype.slice.call(arguments);
+        parent.logEntry([...args]);
+      }\n`
+
+    code = consoleLogRewrite + code;
+    let html = '<html><head>' + '<style>' + '</style></head><body>' + '<script type="text/javascript">' + code + '</script></body></html>';
 
     //writing file in the iframe and compiling
-    doc.open();
-    doc.write(result);
-    doc.close();
+    iframe.contentDocument.open();
+    iframe.contentDocument.write(html);
+    iframe.contentDocument.close();
 }
 
-window.logEntry = function(message) {
-  let node = document.createElement('div');
-  let text = document.createTextNode(message);
-  node.appendChild(text);
-  document.body.appendChild(node);
+function refreshLogs() {
+  //if the consoleLogs node already exists, remove it
+  let consoleLogs = document.getElementById('console-logs');
+  if (consoleLogs) {
+    consoleLogs.parentNode.removeChild(consoleLogs);
+  }
+
+  //(re)create the node
+  consoleLogs = document.createElement('div');
+  consoleLogs.setAttribute('id', 'console-logs');
+  document.body.appendChild(consoleLogs);
+
+  return consoleLogs;
 }
 
-function Execute(props){
-    //creating iframe with id iframe and appending on document
-    let iframe;
-    if(document.getElementById("iframe")) {
-        iframe = document.getElementById("iframe");
-    } else {
-        iframe = document.createElement('iframe');
-        iframe.setAttribute("id","iframe")
-        document.body.appendChild(iframe);
-    }
+window.logEntry = function(logEntries) {
+  const consoleLogs = document.getElementById('console-logs');
 
-    //running code when press Run
-    const butt = <button onClick={()=>myFunc(props.code, iframe)}>Run</button>;
-
-    return <div>{butt}</div>
+  let logEntry = logEntries[0];
+  for (let i = 1; i < logEntries.length; i++) {
+    logEntry = logEntry + " " + logEntries[i];
+  }
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(logEntry));
+  consoleLogs.appendChild(div);
+  document.body.appendChild(consoleLogs);
 }
 
 export default Execute;
-
-// <Iframe id='Ifrane'></Iframe>
-
-// <HtmlBox id='html'></HtmlBox>
-// <CssBox id='css'></CssBox>
-// <JsBox id='js'></JsBox>
