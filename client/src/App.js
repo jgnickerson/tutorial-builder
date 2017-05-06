ce5813fcc01708216bfd58b6807419638c89f0e5
jgnickerson <jgnickerson@me.com>
2017-05-06 00:46:48 -0400

    login and signup work with auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import Login from './components/account/Login.js';
-import Signup from './components/account/Signup.js';
+import AuthContainer from './components/account/AuthContainer.js';
 import TutorialBrowser from './components/browser/TutorialBrowser.js';
 import TutorialContainer from './components/tutorial/TutorialContainer.js';
 import MenuBar from './components/MenuBar.js';
 
 const SERVER = 'http://localhost:4200';
-
-const CenteredTitle=styled.h1`
-  text-align: center;
-`;
 
 class App extends Component {
   constructor(props){
@@ -20,156 +15,25 @@ class App extends Component {
     this.state = {
       mode: 'login',
       activeTutorial: null,
-      activeUser: {
-        username: "",
-        password: "",
-      },
       errorMessage: ""
     };
 
     //ES6 Class/React thing we have to do to make sure this is bound properly...
     this.handleTutorialSelect = this.handleTutorialSelect.bind(this);
     this.handleTutorialExit = this.handleTutorialExit.bind(this);
-    this.attemptLogin = this.attemptLogin.bind(this);
-    this.switchToRegister = this.switchToRegister.bind(this);
-    this.attemptRegister = this.attemptRegister.bind(this);
-    this.changeAccount = this.changeAccount.bind(this);
+    this.switchMode = this.switchMode.bind(this);
   }
 
   handleTutorialSelect(tutorialId) {
-    this.setState({
-      mode: 'tutorial',
-      activeTutorial: tutorialId
-    });
+    this.setState({ mode: 'tutorial', activeTutorial: tutorialId });
   }
 
   handleTutorialExit() {
-    this.setState({
-      mode: 'browser',
-      activeTutorial: null
-    })
+    this.setState({ mode: 'browser', activeTutorial: null, errorMessage: "" })
   }
 
-  attemptLogin() {
-    if (this.state.activeUser.username) {
-      fetch(SERVER + '/users/'+this.state.activeUser.username)
-      .then((response) => {
-        //usernames matched successfully
-        if (response.ok) {
-          return response.json();
-        } else {
-          this.setState({errorMessage: "This username does not exist."});
-        }
-      }).then((serverUser) => {
-        if (serverUser) {
-          //passwords matched
-          if (serverUser.password === this.state.activeUser.password) {
-            this.setState({
-              mode: 'browser',
-              activeUser: serverUser,
-              errorMessage: ""
-            });
-          } else {
-            this.setState({errorMessage: "The password is incorrect."});
-          }
-        }
-      });
-    } else {
-      this.setState({errorMessage: "Please enter your credentials."})
-    }
-  }
-
-  switchToRegister() {
-    this.setState({
-      mode: 'signup',
-      activeUser: {
-        username: "",
-        password: "",
-      },
-      errorMessage: ""
-    });
-  }
-
-  attemptRegister() {
-    // only allow non-empty passwords and non-empty usernames
-    if (this.state.activeUser.password && this.state.activeUser.username) {
-      // if the passwords match
-      if (this.state.activeUser.password === this.state.passCheck) {
-        // put the user's info from the state into an object
-        const newUser = {
-          username: this.state.activeUser.username,
-          password: this.state.activeUser.password,
-          tutorialsUsed: [],
-          tutorialsOwned: []
-        };
-
-        // stringify the object
-        const userStr = JSON.stringify(newUser);
-
-        // define the POST request
-        const postRequest = new Request(
-          SERVER + "/users/",
-          {
-            method:'POST',
-            body: userStr,
-            headers: new Headers({'Content-type': 'application/json'})
-          }
-        );
-
-        // attempt adding the new user to the db
-        fetch(postRequest)
-        .then((response)=>{
-          if (response.ok){
-            return response.json();
-          } else {
-            // the given username already exists
-            this.setState({
-              errorMessage: "The provided username already exists."
-            });
-          }
-        })
-        .then((serverUser) => {
-          if (serverUser) {
-            this.setState({
-              mode: "successfulRegistration"
-            });
-          }
-        });
-
-      } else {
-        this.setState({
-          errorMessage: "The passwords do not match."
-        });
-      }
-
-    } else {
-      // handle error messages for empty usernames and passwords
-      let errorMessage;
-
-      if (!this.state.activeUser.password) {
-        errorMessage = "Please enter a password."
-      }
-
-      if (!this.state.activeUser.username) {
-        errorMessage = "Please enter a username."
-      }
-
-      this.setState({
-        errorMessage: errorMessage
-      });
-    }
-  }
-
-  changeAccount() {
-    this.setState({
-      mode: 'login',
-      activeTutorial: null,
-      activeUser: {
-        username: "",
-        password: "",
-      },
-      errorMessage: ""
-    });
+  switchMode(mode) {
+    this.setState({ mode: mode, errorMessage: "" });
   }
 
   autoSave(modifiedCode, currentStage) {
@@ -220,72 +84,41 @@ class App extends Component {
   render() {
     let activeComponent;
 
-    if (this.state.mode === 'login') {
-      activeComponent = (
-        <div>
-          <CenteredTitle>Tutorial Builder</CenteredTitle>
-          <Login username={this.state.activeUser.username}
-                 password={this.state.activeUser.password}
-                 setUsername={(event) => this.setState({activeUser: {username: event.target.value, password: this.state.activeUser.password }})}
-                 setPassword={(event) => this.setState({activeUser: {username: this.state.activeUser.username, password: event.target.value }})}
-                 attemptLogin={this.attemptLogin}
-                 errorMessage={this.state.errorMessage}
-                 switchToRegister={this.switchToRegister}/>
-        </div>
-      );
-
-    } else if (this.state.mode === 'signup') {
-      activeComponent = (
-        <div>
-          <CenteredTitle>Tutorial Builder</CenteredTitle>
-          <Signup username={this.state.activeUser.username}
-                  password={this.state.activeUser.password}
-                  setUsername={(event) => this.setState({activeUser: {username: event.target.value, password: this.state.activeUser.password }})}
-                  setPassword={(event) => this.setState({activeUser: {username: this.state.activeUser.username, password: event.target.value }})}
-                  errorMessage={this.state.errorMessage}
-                  passCheck={this.state.passCheck}
-                  setPassCheck={(event) => this.setState({passCheck: event.target.value})}
-                  backToLogin={this.changeAccount}
-                  attemptRegister={this.attemptRegister}/>
-        </div>
-      );
-
-    } else if (this.state.mode === 'successfulRegistration') {
-      activeComponent = (
-        <div>
-          <CenteredTitle>Tutorial Builder</CenteredTitle>
+    switch (this.state.mode) {
+      case 'login':
+      case 'signup':
+      case 'signupSuccess':
+        activeComponent = (
           <div>
-            <h2>You have successfully registered!</h2>
-            <br/>
-            <button onClick={this.changeAccount}>Go back to Log In</button>
+            <AuthContainer mode={this.state.mode} switchMode={this.switchMode}/>
           </div>
-        </div>
-      );
+        );
+        break;
 
-    } else if (this.state.mode === 'browser') {
-      activeComponent = (
-        <div>
-          <MenuBar logout={this.changeAccount} browse={this.handleTutorialExit}/>
-          <TutorialBrowser onSelect={this.handleTutorialSelect}/>
-        </div>
-      );
+      case 'browser':
+        activeComponent = (
+          <div>
+            <MenuBar logout={() => this.switchMode('login')} browse={this.handleTutorialExit}/>
+            <TutorialBrowser onSelect={this.handleTutorialSelect}/>
+          </div>
+        );
+        break;
 
-    } else {
-      // if the user selected a tutorial, show it to them
-      activeComponent = (
-        <div>
-          <MenuBar logout={this.changeAccount} browse={this.handleTutorialExit}/>
-          <TutorialContainer onExit={this.handleTutorialExit}
-                             activeTutorial={this.state.activeTutorial}
-                             username={this.state.activeUser.username}
-                             autoSave={(modifiedCode, currentStage) => this.autoSave(modifiedCode, currentStage)}/>;
-        </div>
-      );
+      //mode === 'tutorial'
+      default:
+        activeComponent = (
+          <div>
+            <MenuBar logout={() => this.switchMode('login')} browse={this.handleTutorialExit}/>
+            <TutorialContainer onExit={this.handleTutorialExit}
+                               activeTutorial={this.state.activeTutorial}
+                               autoSave={(modifiedCode, currentStage) => this.autoSave(modifiedCode, currentStage)}/>;
+          </div>
+        );
     }
 
     return (
       <div className="App">
-        {activeComponent}
+        { activeComponent }
       </div>
     )
   }
---
+++ client/src/components/account/AuthContainer.js
@@ -0,0 +1,148 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Login from './Login.js';
+import Signup from './Signup.js';
+
+const CenteredTitle=styled.h1`
+  text-align: center;
+`;
+
+class AuthContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: '',
+      password: '',
+      passwordCheck: '',
+      errorMessage: ''
+    }
+
+    this.attemptLogin = this.attemptLogin.bind(this);
+    this.attemptRegister = this.attemptRegister.bind(this);
+  }
+
+  attemptLogin() {
+    //TODO check for empty password
+    if (this.state.username) {
+      fetch('/login/', {
+        method: 'POST',
+        body: JSON.stringify({ username: this.state.username, password: this.state.password }),
+        headers: new Headers({ 'Content-type': 'application/json' })
+      })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          //TODO handle and set error messages here
+          console.log(response);
+        }
+      }).then((data) => {
+        localStorage.setItem('jwt', data.token);
+        this.props.switchMode('browser');
+      });
+    } else {
+      this.setState({errorMessage: "Please enter your credentials."})
+    }
+  }
+
+  attemptRegister() {
+    let error;
+    if (this.state.password && this.state.username) {
+      if (this.state.password === this.state.passwordCheck) {
+        fetch("/users/",
+        {
+          method:'POST',
+          body: JSON.stringify({ username: this.state.username, password: this.state.password }),
+          headers: new Headers({'Content-type': 'application/json'})
+        })
+        .then((response)=>{
+          if (response.ok){
+            return response.json();
+          } else {
+            console.log(response);
+            //TODO handle various server errors here
+          }
+        })
+        .then((data) => {
+          localStorage.setItem('jwt', data.token);
+          this.props.switchMode('signupSuccess');
+        });
+      } else {
+        error = "The passwords do not match."
+      }
+    } else {
+      if (!this.state.activeUser.password) {
+        error = "Please enter a password."
+      }
+      if (!this.state.activeUser.username) {
+        error = "Please enter a username."
+      }
+      this.setState({
+        errorMessage: error
+      });
+    }
+  }
+
+  onUsernameChange(username) {
+    this.setState({username})
+  }
+
+  onPasswordChange(password) {
+    this.setState({password})
+  }
+
+  onPasswordCheckChange(password) {
+    this.setState({passwordCheck: password})
+  }
+
+  render() {
+    let active;
+
+    switch (this.props.mode) {
+      case 'login':
+        active = (
+          <Login username={this.state.username}
+                 password={this.state.password}
+                 setUsername={(e) => this.setState({username: e.target.value})}
+                 setPassword={(e) => this.setState({password: e.target.value})}
+                 attemptLogin={this.attemptLogin}
+                 errorMessage={this.state.errorMessage}
+                 switchToRegister={() => this.props.switchMode('signup')}/>
+        );
+        break;
+
+      case 'signup':
+        active = (
+          <Signup username={this.state.username}
+                  password={this.state.password}
+                  passwordCheck={this.state.passwordCheck}
+                  setUsername={(e) => this.setState({username: e.target.value})}
+                  setPassword={(e) => this.setState({password: e.target.value})}
+                  setPassCheck={(e) => this.setState({passwordCheck: e.target.value})}
+                  attemptRegister={this.attemptRegister}
+                  errorMessage={this.state.errorMessage}
+                  backToLogin={() => this.props.switchMode('login')}/>
+        );
+        break;
+
+      case 'signupSuccess':
+        active = (
+          <div>
+            <h2>You have successfully registered!</h2>
+            <br/>
+            <button onClick={() => this.props.switchMode('browser')}>Browse Tutorials!</button>
+          </div>
+        );
+    }
+
+    return (
+      <div>
+        <CenteredTitle>Tutorial Builder</CenteredTitle>
+        {active}
+      </div>
+    );
+  }
+}
+
+export default AuthContainer;
